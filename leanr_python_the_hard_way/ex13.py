191dc201a63e6a72f47092be0d74ab7176bcae7a
srgzyq_air <srgzyq@playcrab.com>
2016-01-15 12:44:19 +0800

    add command line argv

---
+++ leanr_python_the_hard_way/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, frist, second, third = argv
+
+print "The script is called:", script
+print "Your frist variable is:", frist
+print "Your second variable is:", second
+print "Your third variable is:", third
